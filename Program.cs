f11429f6bc25c1ef701ecda63745a7dada60054a
alex <mr.alexpetrov66@gmail.com>
2023-11-08 15:25:40 +0200

    +1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int elemetsToPush = input[0];
+int elemetsToPop = input[1];
+int number = input[2];
+
+Queue<int> stack = new();
+
+for (int i = 0; i < elemetsToPush; i++)
+{
+    stack.Enqueue(numbers[i]);
+
+}
+
+for (int i = 0; i < elemetsToPop; i++)
+{
+    stack.Dequeue();
+}
+
+if (stack.Contains(number))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    if (stack.Any())
+    {
+        Console.WriteLine(stack.Min());
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+}
+
+
+
+
+
+
